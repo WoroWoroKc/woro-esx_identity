eeaa8ae606c8cd9385799673b7eae2ce97485d4a
WoroWoro <68477799+WoroWoroKc@users.noreply.github.com>
2021-12-13 19:21:43 +0100

    Add files via upload

---
+++ esx_identity/client/main.lua
@@ -0,0 +1,169 @@
+local guiEnabled = false
+local myIdentity = {}
+local myIdentifiers = {}
+local hasIdentity = false
+local isDead = false
+
+ESX = nil
+
+CreateThread(function()
+    while ESX == nil do
+        TriggerEvent('esx:AkiragetSharedObject', function(obj) ESX = obj end)
+        Citizen.Wait(0)
+    end
+end)
+
+AddEventHandler('esx:onPlayerDeath', function(data)
+    isDead = true
+end)
+
+AddEventHandler('playerSpawned', function(spawn)
+    isDead = false
+end)
+
+function EnableGui(state)
+    SetNuiFocus(state, state)
+    guiEnabled = state
+
+    SendNUIMessage({
+        type = "enableui",
+        enable = state
+    })
+end
+
+RegisterNetEvent('esx_identity:showRegisterIdentity')
+AddEventHandler('esx_identity:showRegisterIdentity', function()
+    if not isDead then
+        EnableGui(true)
+    end
+end)
+
+RegisterNetEvent('esx_identity:identityCheck')
+AddEventHandler('esx_identity:identityCheck', function(identityCheck)
+    hasIdentity = identityCheck
+end)
+
+RegisterNetEvent('esx_identity:saveID')
+AddEventHandler('esx_identity:saveID', function(data)
+    myIdentifiers = data
+end)
+
+RegisterNUICallback('escape', function(data, cb)
+    if hasIdentity then
+        EnableGui(false)
+    else
+        TriggerEvent('chatMessagess','[IDENTITY] ', 1, 'You must create your first character in order to play!')
+    end
+end)
+
+RegisterNUICallback('JEBACTUSKAKURWEFAJNYJESTEMREJESTRACJAPOSTACIHEREJSDJHASHDADASZUBIENICAPESTYCYDYBRONHEREXDDDDDDDDDKARTKYKOCHAMCIE', function(data, cb)
+    local reason = ""
+    myIdentity = data
+    for theData, value in pairs(myIdentity) do
+        if theData == "firstname" or theData == "lastname" then
+            reason = verifyName(value)
+
+            if reason ~= "" then
+                break
+            end
+        elseif theData == "dateofbirth" then
+            if value == "invalid" then
+                reason = "Invalid date of birth!"
+                break
+            end
+        elseif theData == "height" then
+            local height = tonumber(value)
+            if height then
+                if height > 200 or height < 140 then
+                    reason = "Unacceptable player height!"
+                    break
+                end
+            else
+                reason = "Unacceptable player height!"
+                break
+            end
+        end
+    end
+
+    if reason == "" then
+        EnableGui(false)
+        DoScreenFadeOut(1000)
+        DoScreenFadeIn(3000)
+        SetCamActive(cam,  false)
+        RenderScriptCams(false,  false,  0,  true,  true)
+        SetEntityCollision(GetPlayerPed(-1),  true,  true)
+        SetEntityVisible(GetPlayerPed(-1),  true)
+        FreezeEntityPosition(GetPlayerPed(-1), false)
+        DoScreenFadeIn(1000)
+        TriggerServerEvent('esx_identity:setIdentity', data, myIdentifiers)
+        EnableGui(false)
+        ESX.Scaleform.ShowFreemodeMessage('~g~AkiraRP', 'Ustaw się w dogodnym miejscu, za 10 sekund uruchomi się menu tworzenia postaci.', 3)
+        Wait(150)
+        ESX.ShowAdvancedNotification('~g~AkiraRP', nil, 'Menu tworzenia postaci zostało otworzone.', 'CustomLogo')
+        TriggerEvent('esx_skin:openSaveableMenu', myIdentifiers.id)
+    else
+        TriggerEvent('DoLongHudText' ..reason, 1)
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        if guiEnabled then
+            DisableControlAction(0, 1,   true) -- LookLeftRight
+            DisableControlAction(0, 2,   true) -- LookUpDown
+            DisableControlAction(0, 106, true) -- VehicleMouseControlOverride
+            DisableControlAction(0, 142, true) -- MeleeAttackAlternate
+            DisableControlAction(0, 30,  true) -- MoveLeftRight
+            DisableControlAction(0, 31,  true) -- MoveUpDown
+            DisableControlAction(0, 21,  true) -- disable sprint
+            DisableControlAction(0, 24,  true) -- disable attack
+            DisableControlAction(0, 25,  true) -- disable aim
+            DisableControlAction(0, 47,  true) -- disable weapon
+            DisableControlAction(0, 58,  true) -- disable weapon
+            DisableControlAction(0, 263, true) -- disable melee
+            DisableControlAction(0, 264, true) -- disable melee
+            DisableControlAction(0, 257, true) -- disable melee
+            DisableControlAction(0, 140, true) -- disable melee
+            DisableControlAction(0, 141, true) -- disable melee
+            DisableControlAction(0, 143, true) -- disable melee
+            DisableControlAction(0, 75,  true) -- disable exit vehicle
+            DisableControlAction(27, 75, true) -- disable exit vehicle
+        end
+        Citizen.Wait(10)
+    end
+end)
+
+function verifyName(name)
+    local nameLength = string.len(name)
+    if nameLength > 35 or nameLength < 1 then
+        return 'Your player name is either too short or too long.'
+    end
+    local count = 0
+    for i in name:gmatch('[abcdefghijklmnopqrstuvwxyzåäöABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ0123456789 -]') do
+        count = count + 1
+    end
+    if count ~= nameLength then
+        return 'Your player name contains special characters that are not allowed on this server.'
+    end
+
+    local spacesInName    = 0
+    local spacesWithUpper = 0
+    for word in string.gmatch(name, '%S+') do
+
+        if string.match(word, '%u') then
+            spacesWithUpper = spacesWithUpper + 1
+        end
+
+        spacesInName = spacesInName + 1
+    end
+
+    if spacesInName > 2 then
+        return 'Your name contains more than two spaces'
+    end
+
+    if spacesWithUpper ~= spacesInName then
+        return 'your name must start with a capital letter.'
+    end
+
+    return ''
+end
---
+++ esx_identity/server/main.lua
@@ -0,0 +1,260 @@
+ESX= nil
+
+TriggerEvent('esx:AkiragetSharedObject', function(obj) ESX= obj end)
+
+function getIdentity(source, callback)
+	local identifier = ESX.GetPlayerFromId(source).identifier
+
+	MySQL.Async.fetchAll('SELECT identifier, firstname, lastname, dateofbirth, sex, height FROM `users` WHERE `identifier` = @identifier', {
+		['@identifier'] = identifier
+	}, function(result)
+		if result[1].firstname ~= nil then
+			local data = {
+				identifier	= result[1].identifier,
+				firstname	= result[1].firstname,
+				lastname	= result[1].lastname,
+				dateofbirth	= result[1].dateofbirth,
+				sex			= result[1].sex,
+				height		= result[1].height
+			}
+
+			callback(data)
+		else
+			local data = {
+				identifier	= '',
+				firstname	= '',
+				lastname	= '',
+				dateofbirth	= '',
+				sex			= '',
+				height		= ''
+			}
+
+			callback(data)
+		end
+	end)
+end
+
+function getCharacters(source, callback)
+	local identifier = ESX.GetPlayerFromId(source).identifier
+	MySQL.Async.fetchAll('SELECT * FROM `characters` WHERE `identifier` = @identifier', {
+		['@identifier'] = identifier
+	}, function(result)
+		if result[1] and result[2] and result[3] then
+
+			local data = {
+				identifier		= result[1].identifier,
+				firstname1		= result[1].firstname,
+				lastname1		= result[1].lastname,
+				dateofbirth1	= result[1].dateofbirth,
+				sex1			= result[1].sex,
+				height1			= result[1].height,
+				firstname2		= result[2].firstname,
+				lastname2		= result[2].lastname,
+				dateofbirth2	= result[2].dateofbirth,
+				sex2			= result[2].sex,
+				height2			= result[2].height,
+				firstname3		= result[3].firstname,
+				lastname3		= result[3].lastname,
+				dateofbirth3	= result[3].dateofbirth,
+				sex3			= result[3].sex,
+				height3			= result[3].height
+			}
+
+			callback(data)
+
+		elseif result[1] and result[2] and not result[3] then
+
+			local data = {
+				identifier		= result[1].identifier,
+				firstname1		= result[1].firstname,
+				lastname1		= result[1].lastname,
+				dateofbirth1	= result[1].dateofbirth,
+				sex1			= result[1].sex,
+				height1			= result[1].height,
+				firstname2		= result[2].firstname,
+				lastname2		= result[2].lastname,
+				dateofbirth2	= result[2].dateofbirth,
+				sex2			= result[2].sex,
+				height2			= result[2].height,
+				firstname3		= '',
+				lastname3		= '',
+				dateofbirth3	= '',
+				sex3			= '',
+				height3			= ''
+			}
+
+			callback(data)
+
+		elseif result[1] and not result[2] and not result[3] then
+
+			local data = {
+				identifier		= result[1].identifier,
+				firstname1		= result[1].firstname,
+				lastname1		= result[1].lastname,
+				dateofbirth1	= result[1].dateofbirth,
+				sex1			= result[1].sex,
+				height1			= result[1].height,
+				firstname2		= '',
+				lastname2		= '',
+				dateofbirth2	= '',
+				sex2			= '',
+				height2			= '',
+				firstname3		= '',
+				lastname3		= '',
+				dateofbirth3	= '',
+				sex3			= '',
+				height3			= ''
+			}
+
+			callback(data)
+
+		else
+
+			local data = {
+				identifier		= '',
+				firstname1		= '',
+				lastname1		= '',
+				dateofbirth1	= '',
+				sex1			= '',
+				height1			= '',
+				firstname2		= '',
+				lastname2		= '',
+				dateofbirth2	= '',
+				sex2			= '',
+				height2			= '',
+				firstname3		= '',
+				lastname3		= '',
+				dateofbirth3	= '',
+				sex3			= '',
+				height3			= ''
+			}
+
+			callback(data)
+
+		end
+	end)
+end
+
+function setIdentity(identifier, data, source, callback)
+	MySQL.Async.execute('UPDATE `users` SET `firstname` = @firstname, `lastname` = @lastname, `dateofbirth` = @dateofbirth, `sex` = @sex, `height` = @height WHERE identifier = @identifier', {
+		['@identifier']		= identifier,
+		['@firstname']		= data.firstname,
+		['@lastname']		= data.lastname,
+		['@dateofbirth']	= data.dateofbirth,
+		['@sex']			= data.sex,
+		['@height']			= data.height
+	}, function(rowsChanged)
+		if callback then
+			callback(true)
+		end
+	end)
+
+	MySQL.Async.execute('INSERT INTO characters (identifier, firstname, lastname, dateofbirth, sex, height) VALUES (@identifier, @firstname, @lastname, @dateofbirth, @sex, @height)', {
+		['@identifier']		= identifier,
+		['@firstname']		= data.firstname,
+		['@lastname']		= data.lastname,
+		['@dateofbirth']	= data.dateofbirth,
+		['@sex']			= data.sex,
+		['@height']			= data.height
+	})
+end
+
+function updateIdentity(identifier, data, callback)
+	MySQL.Async.execute('UPDATE `users` SET `firstname` = @firstname, `lastname` = @lastname, `dateofbirth` = @dateofbirth, `sex` = @sex, `height` = @height WHERE identifier = @identifier', {
+		['@identifier']		= identifier,
+		['@firstname']		= data.firstname,
+		['@lastname']		= data.lastname,
+		['@dateofbirth']	= data.dateofbirth,
+		['@sex']			= data.sex,
+		['@height']			= data.height
+	}, function(rowsChanged)
+		if callback then
+			callback(true)
+		end
+	end)
+end
+
+function deleteIdentity(identifier, data, callback)
+	MySQL.Async.execute('DELETE FROM `characters` WHERE identifier = @identifier AND firstname = @firstname AND lastname = @lastname AND dateofbirth = @dateofbirth AND sex = @sex AND height = @height', {
+		['@identifier']		= identifier,
+		['@firstname']		= data.firstname,
+		['@lastname']		= data.lastname,
+		['@dateofbirth']	= data.dateofbirth,
+		['@sex']			= data.sex,
+		['@height']			= data.height
+	}, function(rowsChanged)
+		if callback then
+			callback(true)
+		end
+	end)
+end
+
+RegisterServerEvent('esx_identity:setIdentity')
+AddEventHandler('esx_identity:setIdentity', function(data, myIdentifiers)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	setIdentity(xPlayer.identifier, data, source, function(callback)
+		if callback then
+			TriggerClientEvent('esx_identity:identityCheck', xPlayer.source, true)
+		end
+	end)
+end)
+
+AddEventHandler('esx:AkiraplayerLoaded', function(source)
+	local myID = {
+		steamid = ESX.GetPlayerFromId(source).identifier,
+		playerid = source
+	}
+
+	TriggerClientEvent('esx_identity:saveID', source, myID)
+	getIdentity(source, function(data)
+		if data.firstname == '' then
+			TriggerClientEvent('esx_identity:identityCheck', source, false)
+			TriggerClientEvent('esx_identity:showRegisterIdentity', source)
+		else
+			TriggerClientEvent('esx_identity:identityCheck', source, true)
+		end
+	end)
+end)
+
+AddEventHandler('onResourceStart', function(resource)
+	if resource == GetCurrentResourceName() then
+		Citizen.Wait(3000)
+
+		local xPlayers, xPlayer = ESX.GetPlayers()
+		for i=1, #xPlayers, 1 do
+			xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+
+			local myID = {
+				steamid  = xPlayer.identifier,
+				playerid = xPlayer.source
+			}
+
+			TriggerClientEvent('esx_identity:saveID', xPlayer.source, myID)
+
+			getIdentity(xPlayer.source, function(data)
+				if data.firstname == '' then
+					TriggerClientEvent('esx_identity:identityCheck', xPlayer.source, false)
+					TriggerClientEvent('esx_identity:showRegisterIdentity', xPlayer.source)
+				else
+					TriggerClientEvent('esx_identity:identityCheck', xPlayer.source, true)
+				end
+			end)
+		end
+	end
+end)
+
+
+RegisterCommand('register', function(source, args, rawcommand)
+	if source == 0 then
+		TriggerClientEvent('esx_identity:showRegisterIdentity', args[1], true)
+	else
+		local xPlayer = ESX.GetPlayerFromId(source)
+		if xPlayer.group == 'best' or xPlayer.group == 'superadmin' or xPlayer.group == 'admin' or xPlayer.group == 'moderator' then
+			TriggerClientEvent('esx_identity:showRegisterIdentity', args[1], true)
+			xPlayer.showNotification('Otwarto menu tworzenia postaci dla ~g~ ' .. args[1])
+		else
+			xPlayer.showNotification('~r~Nie posiadasz permisji!')
+		end
+	end
+end, true)
